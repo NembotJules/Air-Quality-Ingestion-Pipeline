a5a95b1bd4e00fcf7d2905e3b98c29df7b8d41c6
Nembot Jules <nembotjulesgithub@gmail.com>
2024-09-16 19:06:12 +0100

    Updated the ingestion file

--- pipelines/data_ingestion.py
+++ pipelines/data_ingestion.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 import os
-from prefect import flow, task
+from prefect import flow, task, variables
 from prefect.tasks import task_input_hash
 from datetime import timedelta
 from pathlib import Path
